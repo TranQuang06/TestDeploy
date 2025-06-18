6fb6295ddd88f6b4489aad50d2aa7069dd2e2f25
TranQuang06 <quangtnn.24ce@vku.udn.vn>
2025-06-19 01:20:35 +0700

    Hahahaahahahahaahah

--- src/pages/DynamicCV/index.js
+++ src/pages/DynamicCV/index.js
@@ -1,20 +1,31 @@
 // pages/DynamicCV/index.js
 
-import React, { useState, useEffect, memo } from "react";
+import React, { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
-import MainLayout from "../../components/layout/MainLayout";
-import Loading from "../../components/ui/Loading";
+import Header from "../../components/Header";
+import { Spin } from "antd";
 
 // Dynamic import để tránh lỗi SSR
 const CVTemplateDetailed = dynamic(
   () => import("../../components/CVTemplateDetail"),
   {
     ssr: false,
-    loading: () => <Loading.Page message="Đang tải CV template..." />,
+    loading: () => (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "400px",
+        }}
+      >
+        <Spin size="large" />
+      </div>
+    ),
   }
 );
 
-const DynamicCV = memo(() => {
+export default function DynamicCV() {
   const [cvData, setCvData] = useState(null);
   const [isClient, setIsClient] = useState(false);
 
@@ -51,21 +62,41 @@ const DynamicCV = memo(() => {
       });
   }, []);
 
+  if (!isClient) {
+    return (
+      <>
+        <Header />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "400px",
+          }}
+        >
+          <Spin size="large" />
+        </div>
+      </>
+    );
+  }
+
   return (
-    <MainLayout
-      title="Tạo CV động"
-      description="Tạo CV chuyên nghiệp với template động và tùy chỉnh theo nhu cầu"
-      keywords="tạo CV, CV template, CV chuyên nghiệp, thiết kế CV"
-    >
-      {!isClient || !cvData ? (
-        <Loading.Page message="Đang tải CV template..." />
-      ) : (
+    <>
+      <Header />
+      {cvData ? (
         <CVTemplateDetailed data={cvData} />
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "400px",
+          }}
+        >
+          <Spin size="large" />
+        </div>
       )}
-    </MainLayout>
+    </>
   );
-});
-
-DynamicCV.displayName = "DynamicCV";
-
-export default DynamicCV;
+}
